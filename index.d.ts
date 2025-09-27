315b62bfa1525cc119fb7610d489dbf70a655f02
Zeeshan Abbasi <zeeshanabbasi313@gmail.com>
2025-09-28 04:27:20 +0500

    remove issues

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-import TikTokEmbedPlayer from './src/TikTokEmbedPlayer';
-export default TikTokEmbedPlayer;
-export { TikTokEmbedPlayer };
-export type { TikTokEmbedRef } from './src/TikTokEmbedPlayer';
