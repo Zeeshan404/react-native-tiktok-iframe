9349379428683e713d4df2825d3a2d7ad796e6ae
Zeeshan Abbasi <zeeshanabbasi313@gmail.com>
2025-09-28 04:17:49 +0500

    update auto import fix

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+import TikTokEmbedPlayer from './src/TikTokEmbedPlayer';
+export default TikTokEmbedPlayer;
+export { TikTokEmbedPlayer };
+export type { TikTokEmbedRef } from './src/TikTokEmbedPlayer';
